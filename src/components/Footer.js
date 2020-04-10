91d96603261413c140ebf31f6fcbe83ddefdc849
hellogareth <hello@gareth.work>
2020-04-10 13:19:27 +0700

    Previous commit deleted files, this adds Gatsby

---
+++ src/components/Footer.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import logo from '../img/logo.svg'
+import facebook from '../img/social/facebook.svg'
+import instagram from '../img/social/instagram.svg'
+import twitter from '../img/social/twitter.svg'
+import vimeo from '../img/social/vimeo.svg'
+
+const Footer = class extends React.Component {
+  render() {
+    return (
+      <footer className="footer has-background-black has-text-white-ter">
+        <div className="content has-text-centered">
+          <img
+            src={logo}
+            alt="Mutual Aid"
+            width="88"
+          />
+        </div>
+        <div className="content has-text-centered has-background-black has-text-white-ter">
+          <div className="container has-background-black has-text-white-ter">
+            <div className="columns">
+              <div className="column is-4">
+                <section className="menu">
+                  <ul className="menu-list">
+                    <li>
+                      <Link to="/" className="navbar-item">
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/about">
+                        About
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/contact/examples">
+                        Form Examples
+                      </Link>
+                    </li>
+                    <li>
+                      <a
+                        className="navbar-item"
+                        href="/admin/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Admin
+                      </a>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+              <div className="column is-4">
+                <section>
+                  <ul className="menu-list">
+                    <li>
+                      <Link className="navbar-item" to="/blog">
+                        Latest Stories
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/contact">
+                        Contact
+                      </Link>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+              <div className="column is-4 social">
+                <a title="facebook" href="https://facebook.com">
+                  <img
+                    src={facebook}
+                    alt="Facebook"
+                    width="50"
+                  />
+                </a>
+                <a title="twitter" href="https://twitter.com">
+                  <img
+                    className="fas fa-lg"
+                    src={twitter}
+                    alt="Twitter"
+                    width="50"
+                  />
+                </a>
+                <a title="instagram" href="https://instagram.com">
+                  <img
+                    src={instagram}
+                    alt="Instagram"
+                    width="50"
+                  />
+                </a>
+                <a title="vimeo" href="https://vimeo.com">
+                  <img
+                    src={vimeo}
+                    alt="Vimeo"
+                    width="50"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
---
+++ src/pages/blog/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Layout from '../../components/Layout'
+import BlogRoll from '../../components/BlogRoll'
+
+export default class BlogIndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div className="full-width-image-container">
+          <h1>
+            Latest Stories
+          </h1>
+        </div>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <BlogRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
