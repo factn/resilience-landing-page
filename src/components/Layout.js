e470a80a3d6538bf913baf62e78d05656411c0b9
hellogareth <hello@gareth.work>
2020-04-16 11:18:16 +0700

    Changes logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,28 +17,27 @@ const Layout = ({ children }) => {
         <meta name="description" content={description} />
 
 
-
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+          href={`${withPrefix('/')}img/favicon/apple-touch-icon.png`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          href={`${withPrefix('/')}img/favicon/favicon-32x32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          href={`${withPrefix('/')}img/favicon/favicon-16x16.png`}
           sizes="16x16"
         />
 
         <link
           rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+          href={`${withPrefix('/')}img/favicon/safari-pinned-tab.svg`}
           color="#3739b5"
         />
         <meta name="theme-color" content="#fff" />
