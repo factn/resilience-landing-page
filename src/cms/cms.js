f0c36987ea7935f1d74287f4c31d8d1c5b8e62d8
Joshua Wu <joshua.wu.0910@gmail.com>
2020-05-28 18:39:04 -0700

    chore(storiespreview): fix file rename typo

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,12 +3,12 @@ import uploadcare from 'netlify-cms-media-library-uploadcare';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import AboutUsPagePreview from './preview-templates/AboutUsPagePreview';
-import StoriesPostPreview from './preview-templates/StoriesPostPreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import StoriesPostPreview from './preview-templates/StoriesPostPreview';
+import IndexPagePreview from './preview-templates/IndexPagePreview';
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('about-us', AboutUsPagePreview);
-CMS.registerPreviewTemplate('stories', StoriesPostPreview)
+CMS.registerPreviewTemplate('stories', StoriesPostPreview);
