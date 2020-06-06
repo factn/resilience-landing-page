391bb68a0eddc1c56ca6d7873f64f6dc44fad948
Joshua Wu <joshua.wu.0910@gmail.com>
2020-06-05 19:12:26 -0700

    feat: add footer layout and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,59 +1,69 @@
-import React from 'react';
 import { Link } from 'gatsby';
+import React from 'react';
+import { EXTERNAL_LINKS } from '../utils/constants';
+import ExternalLink from './ExternalLink';
 
-import facebook from '../img/social/facebook.svg';
-import instagram from '../img/social/instagram.svg';
-import twitter from '../img/social/twitter.svg';
-import vimeo from '../img/social/vimeo.svg';
-
-const Footer = class extends React.Component {
-  render() {
-    return (
-      <footer className="siteFooter">
-        <nav className="siteNav siteFooter-nav">
-          <Link to="/" className="navbar-item">
-            Home
-          </Link>
-          <Link className="navbar-item" to="/about-us">
+const Footer = () => (
+  <footer className="siteFooter">
+    <div className="responsiveFlex siteContent">
+      <div className="siteFooter_section">
+        <img alt="Resilience Logo" className="logo" src="" />
+      </div>
+      <div className="siteFooter_section">
+        <div className="caps siteFooter_section-title">Resilience</div>
+        <div className="siteFooter_section-item">
+          <Link className="inverse" to="/about-us">
             About Us
           </Link>
-          <Link className="navbar-item" to="/stories">
-            Blog
+        </div>
+        <div className="siteFooter_section-item">
+          <Link className="inverse" to="/faq">
+            FAQ
           </Link>
-        </nav>
-
-        {/* <a title="facebook" href="https://facebook.com">
-          <img
-            src={facebook}
-            alt="Facebook"
-            width="30"
-          />
-        </a>
-        <a title="twitter" href="https://twitter.com">
-          <img
-            className="fas fa-lg"
-            src={twitter}
-            alt="Twitter"
-            width="30"
-          />
-        </a>
-        <a title="instagram" href="https://instagram.com">
-          <img
-            src={instagram}
-            alt="Instagram"
-            width="30"
-          />
-        </a>
-        <a title="vimeo" href="https://vimeo.com">
-          <img
-            src={vimeo}
-            alt="Vimeo"
-            width="30"
-          />
-        </a> */}
-      </footer>
-    );
-  }
-};
+        </div>
+        <div className="siteFooter_section-item">
+          <Link className="inverse" to="/stories">
+            Stories
+          </Link>
+        </div>
+      </div>
+      <div className="siteFooter_section">
+        <div className="caps siteFooter_section-title">Partners</div>
+        <div className="siteFooter_section-item">
+          <Link className="inverse" to="/get-resilience">
+            Get Resilience
+          </Link>
+        </div>
+        <div className="siteFooter_section-item">
+          <ExternalLink className="inverse" href={EXTERNAL_LINKS.faction}>
+            Faction
+          </ExternalLink>
+        </div>
+      </div>
+      <div className="siteFooter_section">
+        <div className="caps siteFooter_section-title">Contribute</div>
+        <div className="siteFooter_section-item">
+          <ExternalLink className="inverse" href={EXTERNAL_LINKS.join}>
+            Join the team
+          </ExternalLink>
+        </div>
+        <div className="siteFooter_section-item">
+          <ExternalLink className="inverse" href={EXTERNAL_LINKS.docs}>
+            Documentation
+          </ExternalLink>
+        </div>
+      </div>
+      <div className="siteFooter_section">
+        <div className="caps siteFooter_section-title">Contact Us</div>
+      </div>
+    </div>
+    <div className="centered">
+      Open Source license -{' '}
+      <ExternalLink className="inverse" href={EXTERNAL_LINKS.license}>
+        licensed under GPL
+      </ExternalLink>
+    </div>
+  </footer>
+);
 
 export default Footer;
