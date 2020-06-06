ced4ccfa7ade569183fca9eb974e5d620e4cb9bb
Joshua Wu <joshua.wu.0910@gmail.com>
2020-06-05 20:01:51 -0700

    refactor(constants): update constants object shape - remove key coersion

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,15 @@
 export const EXTERNAL_LINKS = {
-  ['docs']: 'https://app.gitbook.com/@mutualaidworld/s/docs/',
-  ['faction']: 'https://github.com/factn',
-  ['figma']: 'https://www.figma.com/file/GLg0zPNnwMEGIb9TytCrxQ/Resilience-App',
-  ['join']: 'https://app.gitbook.com/@mutualaidworld/s/docs/local-resilience-app',
-  ['github']: 'https://github.com/factn/resilience-app',
-  ['license']: 'https://github.com/factn/resilience-app/blob/master/LICENSE',
-  ['slack']: 'https://app.slack.com/client/T010BELED7Z',
+  docs: 'https://app.gitbook.com/@mutualaidworld/s/docs/',
+  faction: 'https://github.com/factn',
+  figma: 'https://www.figma.com/file/GLg0zPNnwMEGIb9TytCrxQ/Resilience-App',
+  github: 'https://github.com/factn/resilience-app',
+  join: 'https://app.gitbook.com/@mutualaidworld/s/docs/local-resilience-app',
+  license: 'https://github.com/factn/resilience-app/blob/master/LICENSE',
+  slack: 'https://app.slack.com/client/T010BELED7Z',
 };
+
+const constants = {
+  EXTERNAL_LINKS,
+};
+
+export default constants;
