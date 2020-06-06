85115b1eeb5d51f56c28813639dbd8a1d47ec4ee
Joshua Wu <joshua.wu.0910@gmail.com>
2020-06-05 19:11:16 -0700

    feat: add ExternalLink component, add constants util

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const EXTERNAL_LINKS = {
+  ['docs']: 'https://app.gitbook.com/@mutualaidworld/s/docs/',
+  ['faction']: 'https://github.com/factn',
+  ['figma']: 'https://www.figma.com/file/GLg0zPNnwMEGIb9TytCrxQ/Resilience-App',
+  ['join']: 'https://app.gitbook.com/@mutualaidworld/s/docs/local-resilience-app',
+  ['github']: 'https://github.com/factn/resilience-app',
+  ['license']: 'https://github.com/factn/resilience-app/blob/master/LICENSE',
+  ['slack']: 'https://app.slack.com/client/T010BELED7Z',
+};
